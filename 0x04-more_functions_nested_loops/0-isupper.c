58ccd6536128d568437efccbe9a22b3a8652cad5
kasshunT <ktilahun152@gmail.com>
2022-03-17 01:21:03 -0700

    Write a function that checks for uppercase character

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,18 +1,16 @@
-#include "main.h"
+#include "holberton.h"
 
 /**
-* _isdigit - check digit.
-* @c: integer
-* Return: 1 succes
-*/
-int _isdigit(int c)
+ * _isupper - check uppercase character.
+ * @c: integer.
+ * Return: 0 succes.
+ */
+int _isupper(int c)
 {
-	if ((c >= 48) && (c <= 57))
+	if ((c >= 65) && (c <= 90))
 	{
 		return (1);
 	}
 	else
-	{
 		return (0);
-	}
 }
