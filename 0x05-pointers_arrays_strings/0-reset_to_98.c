4eb20c5f8a9243adb050f85afcfac93a57a79cf3
kasshunT <ktilahun152@gmail.com>
2022-03-21 03:32:48 -0700

    Write a function that takes a pointer to an int as parameter and updates the value it points to to 98.

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,5 +1,11 @@
-#include"main.h"
+#include "main.h"
+
+/**
+ * reset_to_98 - change the value of integer using pointer
+ * @n: pointer
+ * Return:void
+ */
 void reset_to_98(int *n)
 {
-	*n=98;
+	*n = 98;
 }
